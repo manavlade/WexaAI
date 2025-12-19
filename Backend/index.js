72555fb1ae1e95a7aab7e7a85e2f90eec4bd14e1
manavlade <manavlade14690@gmail.com>
2025-12-19 15:37:22 +0530

    backend setup done

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
