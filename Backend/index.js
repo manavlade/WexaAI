760b5dff0fd86300f2e9bf192e30551279359a4e
manavlade <manavlade14690@gmail.com>
2025-12-29 16:11:00 +0530

    added deployed frontend url in backend

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: [
+        'http://localhost:5173',
+        "https://wexa-ai-xi.vercel.app/"
+    ],
+    
     credentials: true
 }
 app.use(cors(corsOptions));
